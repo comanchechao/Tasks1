358d98b9f20643bddac723603bd7c392306a2477
Chao <61477751+comanchechao@users.noreply.github.com>
2024-06-11 22:51:07 +0330

    Update

--- pages/TaskOne.vue
+++ pages/TaskOne.vue
@@ -4,7 +4,7 @@
     class="h-screen w-screen flex justify-center items-center bg-gray-200"
   >
     <div
-      class="h-rem40 w-rem28 flex-col space-y-3 bg-mainWhite flex items-end justify-start rounded-md"
+      class="h-rem45 w-rem28 flex-col space-y-3 drop-shadow-lg bg-mainWhite flex items-end justify-start rounded-md"
     >
       <div class="flex items-center space-x-3 m-2">
         <h1 class="text-md text-mainBlue">انتخاب کوین</h1>
@@ -51,10 +51,58 @@
         <h2 class="text-sm">تغییر <span class="mx-1">24</span> ساعته</h2>
         <h2 class="text-sm">قیمت</h2>
       </div>
+      <div class="my-5 w-full h-full">
+        <div
+          v-for="item in coins.slice(0, 10)"
+          :key="item.name"
+          class="grid grid-cols-3 w-full my-3 hover:bg-gray-100 place-content-center content-center place-items-center"
+        >
+          <div class="flex w-full pl-7 items-start justify-start space-x-2">
+            <img
+              :src="item.icon"
+              :key="item.name"
+              class="object-contain w-7"
+              alt=""
+            />
+            <h2 class="text-sm text-mainBlack">{{ item.name }}</h2>
+          </div>
+          <h2
+            :class="{
+              'bg-green-100 text-green-500': parseFloat(item.change) > 0,
+              'bg-red-100 text-red-500': parseFloat(item.change) < 0,
+            }"
+            class="px-4 py-2 rounded-md"
+          >
+            {{ item.change }}
+          </h2>
+          <h2 class="text-sm py-2 text-mainBlack rounded-md">
+            {{ item.price }}
+          </h2>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script setup>
 import { PhMagnifyingGlass, PhXCircle, PhCoins } from "@phosphor-icons/vue";
+import { ref } from "vue";
+
+const coins = ref([
+  {
+    name: "SOL",
+    icon: "/SOL.webp",
+    change: "0.22%",
+    price: "11309 USD",
+  },
+  { name: "BTC", icon: "/BTC.webp", change: "0.73%", price: "43215.00 USDT" },
+  { name: "ETH", icon: "/Eth.webp", change: "-5.16%", price: "2283.60 USDT" },
+  { name: "OP", icon: "/OP.webp", change: "-2.99%", price: "3.567 USDT" },
+  { name: "AVAX", icon: "/Avax.webp", change: "0.44%", price: "47.53 USDT" },
+  { name: "DOT", icon: "/Dot.webp", change: "6.84%", price: "9.128 USDT" },
+  { name: "XRP", icon: "/Xrp.webp", change: "0.28%", price: "0.6164 USDT" },
+  { name: "ARB", icon: "/Arb.webp", change: "2.05%", price: "13839 USDT" },
+  { name: "NEAR", icon: "/Near.webp", change: "0.82%", price: "30920 USDT" },
+  { name: "MATIC", icon: "/matic.webp", change: "2.86%", price: "0.8697 USDT" },
+  // Add more coins here if needed
+]);
 </script>
-<style></style>
