96ffe90a779f08436e185553d65c7a181f58eda6
Chao <61477751+comanchechao@users.noreply.github.com>
2024-06-12 03:22:39 +0330

    Update Freq.vue

--- components/Freq.vue
+++ components/Freq.vue
@@ -1,18 +1,22 @@
 <template>
   <div
-    v-for="(faq, index) in faqs"
-    :key="index"
+    v-for="faq in faqs"
+    :key="faq.id"
     class="w-full h-full justify-start items-end space-y-4 mt-24 flex-col flex"
   >
     <div class="flex items-center justify-center space-x-2">
-      <h2 class="text-3xl Bold text-mainBlack">
-        {{ faq.category }}
-      </h2>
+      <h2 class="text-3xl Bold text-mainBlack">{{ faq.category }}</h2>
       <img src="../assets/images/logo.svg" class="w-8 object-contain" alt="" />
     </div>
-    <h2 class="text-2xl font-bold text-mainBlack">احراز هویت</h2>
+    <h2
+      v-for="(category, index) in faq.children"
+      :key="index"
+      class="text-2xl font-bold text-mainBlack"
+    >
+      {{ category.category }}
+    </h2>
     <div
-      v-for="(question, index) in faq.children"
+      v-for="(question, index) in faq.children[0].rows"
       :key="index"
       dir="rtl"
       class="collapse"
@@ -22,25 +26,32 @@
         {{ question.question }}
       </div>
       <div class="collapse-content text-mainBlack">
-        <p>{{ question.answer }}</p>
+        {{ question.answer }}
       </div>
     </div>
   </div>
 </template>
 <script setup>
-import { PhMagnifyingGlass, PhXCircle, PhCoins } from "@phosphor-icons/vue";
-
 import { useAsyncData } from "#app"; // Import useAsyncData hook
 
-const { faqs } = useAsyncData("faqData", async () => {
+const faqsData = await useAsyncData("faqData", async () => {
   try {
     const response = await fetch(
       "https://azapi.ok-ex.io/server/api/support/faq"
     );
-    return await response.json();
+    const data = await response.json();
+
+    console.log("Fetched FAQs successfully:", data);
+    return data;
   } catch (error) {
     console.error("Error fetching FAQs:", error);
     return []; // Return an empty array in case of error
   }
 });
+const faqs = faqsData.data._rawValue.data;
+
+console.log(
+  "Fetched FAQs successfully222:",
+  faqs[0].children[0].rows[0].question
+);
 </script>
